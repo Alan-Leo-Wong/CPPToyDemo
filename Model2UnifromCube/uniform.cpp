22c85f93bfa71ad2f10b0ba4d2f2787e542f457f
Poet <602289550@qq.com>
2023-05-05 22:42:15 +0800

    add some example

--- Model2UnifromCube/uniform.cpp
+++ Model2UnifromCube/uniform.cpp
@@ -2,7 +2,7 @@
  * @Author: Poet 602289550@qq.com
  * @Date: 2023-04-10 22:34:06
  * @LastEditors: Poet 602289550@qq.com
- * @LastEditTime: 2023-04-24 16:39:34
+ * @LastEditTime: 2023-05-05 14:15:07
  * @FilePath: \OBJLoader\IN_OBJ.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置
  * 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
