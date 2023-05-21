da19c73219d6241b72144a85358611b2f4d99f0a
Poet <602289550@qq.com>
2023-05-21 22:55:06 +0800

    add compute tri quality

--- OBJLoaderAndTriQualiy/in_obj.cpp
+++ OBJLoaderAndTriQualiy/in_obj.cpp
@@ -2,7 +2,7 @@
  * @Author: Poet 602289550@qq.com
  * @Date: 2023-04-10 22:34:06
  * @LastEditors: Poet 602289550@qq.com
- * @LastEditTime: 2023-04-14 15:49:49
+ * @LastEditTime: 2023-05-21 15:31:06
  * @FilePath: \OBJLoader\IN_OBJ.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置
  * 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
@@ -12,6 +12,7 @@
 #include <cstdio>
 #include <fstream>
 #include <iostream>
+#include <limits>
 #include <sstream>
 #include <vector>
 
@@ -23,7 +24,11 @@ using V3i = Eigen::Vector3i;
 std::vector<V3d> points;
 std::vector<V3i> idx2Points;
 int cnt;
-double angleThreshold = 10.0;
+double angleThreshold = 30.0;
+double avgG = .0;
+double minG = std::numeric_limits<double>::max();
+double minTheta = std::numeric_limits<double>::max();
+double avgMinTheta = .0;
 
 double cpSquare(const V3d &p0, const V3d &p1, const V3d &p2) {
   return 0.5 * (((p1 - p0).cross(p2 - p0)).norm());
@@ -36,6 +41,8 @@ double cpCircum(const V3d &p0, const V3d &p1, const V3d &p2) {
 double cpQuality(const V3d &p0, const V3d &p1, const V3d &p2) {
   double coef = 6 / std::sqrt(3);
   double St = cpSquare(p0, p1, p2);
+  double totalArea = .0;
+  totalArea += St;
   double pt = cpCircum(p0, p1, p2) * 0.5;
 
   double ht =
@@ -102,24 +109,40 @@ int main() {
     if (minAngle < angleThreshold)
       ++cnt;
 
-    double q = cpQuality(points[idx(0)], points[idx(1)], points[idx(2)]);
-    if (0 < q && q <= 0.2)
-      prop[0]++;
-    else if (0.2 < q && q <= 0.4)
-      prop[1]++;
-    else if (0.4 < q && q <= 0.6)
-      prop[2]++;
-    else if (0.6 < q && q <= 0.8)
-      prop[3]++;
-    else if (0.8 < q && q <= 1)
-      prop[4]++;
+    double G = cpQuality(points[idx(0)], points[idx(1)], points[idx(2)]);
+    avgG += G;
+    minG = std::min(minG, G);
+
+    minTheta = std::min(minTheta, minAngle);
+    avgMinTheta += minTheta;
+
+    // if (0 < G && G <= 0.2)
+    //   prop[0]++;
+    // else if (0.2 < G && G <= 0.4)
+    //   prop[1]++;
+    // else if (0.4 < G && G <= 0.6)
+    //   prop[2]++;
+    // else if (0.6 < G && G <= 0.8)
+    //   prop[3]++;
+    // else if (0.8 < G && G <= 1)
+    //   prop[4]++;
   }
 
-  std::cout << "cnt = " << cnt << std::endl;
-  for (int i = 0; i < 5; ++i) {
-    prop[i] /= triFaces;
-    std::cout << prop[i] << std::endl;
-  }
+  // for (int i = 0; i < 5; ++i) {
+  //   prop[i] /= triFaces;
+  //   std::cout << prop[i] << std::endl;
+  // }
+
+  std::cout
+      << "-- The number of triangles with their minimal angles smaller than "
+      << angleThreshold << " is " << cnt
+      << ", percentage = " << (cnt * 100.0 / triFaces) << "%" << std::endl;
+
+  std::cout << "-- G_min = " << minG << std::endl;
+  std::cout << "-- G_avg = " << avgG << std::endl;
+  // std::cout << "-- G_area = " << minG << std::endl;
+  std::cout << "-- Thtea_min = " << minTheta << std::endl;
+  std::cout << "-- Thtea_avg = " << avgMinTheta << std::endl;
 
   in.close();
   return 0;
